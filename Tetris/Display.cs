902fec9a4f4f64757e0057443116d7d58d0a83cf
Angelo Grabner <angelo.ggrabner@gmx.de>
2022-03-29 22:10:26 +0200

    stuff for drawing

--- Tetris/Display.cs
+++ Tetris/Display.cs
@@ -1,5 +1,8 @@
 ﻿public static class Display // to be added
 {
+    private static NewColors color = new();
+    private static char fullBlock = '\u2588'; 
+    private static char[,] s_input = new char[0,0];
     private static CHAR_INFO[] f;
     private static int width = 10*2+1, height = 20;
     static Display()
@@ -10,6 +13,7 @@
 
     public static void update(char[,] input)
     {
+        s_input = input;
         draw();
         ColorSupport.displayFrame(f);
     }
@@ -25,6 +29,47 @@
             f[i].UnicodeChar = '\n';
         }
 
+        for (int x = 0; x < s_input.GetLength(0); x++)
+        {
+            for (int y = 0; y < s_input.GetLength(1); y++)
+            {
+                if (s_input[x,y] != ' ')
+                {
+                    f[convert(x * 2, y)].UnicodeChar = fullBlock; //left side 
+                    f[convert((x * 2) + 1, y)].UnicodeChar = fullBlock; // right side 
+                    switch (s_input[x,y])
+                    {
+                        case 'I':
+                            color = NewColors.lightBlue;
+                            break;
+                        case 'J':
+                            color = NewColors.pink;
+                            break;
+                        case 'L':
+                            color = NewColors.orange;
+                            break;
+                        case 'O':
+                            color= NewColors.yellow;
+                            break;
+                        case 'S':
+                            color = NewColors.red;
+                            break;
+                        case 'Z':
+                            color = NewColors.green;
+                            break;
+                        case 'T':
+                            color = NewColors.purple;
+                            break;
+                        case 'X':
+                        default:
+                            color = NewColors.white;
+                            break;
+                    }
+                    f[convert(x * 2, y)].Attributes = (ushort)color;
+                    f[convert((x * 2)+1, y)].Attributes = (ushort)color;
+                }
+            }
+        }
     }
     private static int convert(int x, int y) // makes a 2d coordinate into a 1d one 
     {
